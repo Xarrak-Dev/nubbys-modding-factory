56cc14c46a6f95187e3ceb4441b725ed6ebb7a89
Xarrak-Dev <xarrak99@gmail.com>
2025-06-07 17:36:56 -0700

    added comments and a label thging

--- node_2d.gd
+++ node_2d.gd
@@ -2,33 +2,45 @@ extends Node2D
 
 var game_path = ""
 var mod_path = ""
+var mods = {}
 
-# Called when the node enters the scene tree for the first time.
+#TODO: move this code to elsewhere so that it isnt trying to connect immediately,
+#probably have it connect to a button or something
 func _ready() -> void:
+	#TODO: change the ip and port to the server ip and port
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_client("127.0.0.1", 8080)
 	multiplayer.multiplayer_peer = peer
 
+#download mod button
+#TODO: just change this system entirely man
 func _on_button_2_pressed() -> void:
 	get_mods.rpc_id(1)
 	download_mod($TextEdit.text)
 
+#get a dictionary from a json file
 func get_json(path):
 	var file = FileAccess.open(path, FileAccess.READ).get_as_text()
 	var json = JSON.new()
 	return JSON.parse_string(file)
 
+#download a mod using the id from the mods.json
+#TODO: add error checking because there is none
+#TODO: check for mods already installed and stuff like that
 func download_mod(mod):
 	var file = get_json(mod_path + "/mods.json")
 	var link = file["mods"][mod]["download"]
+	var id = file["mods"][mod]["id"]
 	var http : HTTPRequest = $HTTPRequest
-	http.download_file = mod_path + "/" + file["mods"][mod]["id"] + ".zip"
+	http.download_file = mod_path + "/" + id + ".zip"
 	http.request(link)
 	await $HTTPRequest.request_completed
 	print("example mod downloaded")
-	extract_all_from_zip(mod_path + "/" + file["mods"][mod]["id"] + ".zip")
-	DirAccess.remove_absolute(mod_path + "/" + file["mods"][mod]["id"] + ".zip")
+	extract_all_from_zip(mod_path + "/" + id + ".zip")
+	DirAccess.remove_absolute(mod_path + "/" + id + ".zip")
 
+#downloads and installs nubby's forgery, deletes it after/ updates nubby's forgery
+#TODO: install custom forgery versions
 func setupForgery():
 	var file = get_json(mod_path + "/mods.json")
 	var httpp : HTTPRequest = $HTTPRequest3
@@ -55,22 +67,20 @@ func setupForgery():
 	DirAccess.remove_absolute(game_path + "/temp_data.win")
 	print("temp data.win removed")
 
+#some code i ripped from the godot tutorials page to unzip the mod file
 func extract_all_from_zip(path):
 	var reader = ZIPReader.new()
 	reader.open(path)
-
 	# Destination directory for the extracted files (this folder must exist before extraction).
 	# Not all ZIP archives put everything in a single root folder,
 	# which means several files/folders may be created in `root_dir` after extraction.
 	var root_dir = DirAccess.open(mod_path)
-
 	var files = reader.get_files()
 	for file_path in files:
 		# If the current entry is a directory.
 		if file_path.ends_with("/"):
 			root_dir.make_dir_recursive(file_path)
 			continue
-
 		# Write file contents, creating folders automatically when needed.
 		# Not all ZIP archives are strictly ordered, so we need to do this in case
 		# the file entry comes before the folder entry.
@@ -79,13 +89,18 @@ func extract_all_from_zip(path):
 		var buffer = reader.read_file(file_path)
 		file.store_buffer(buffer)
 
+#gets the list of mods from the server
 @rpc("any_peer", "call_remote", "reliable")
 func get_mods(data):
 	var file = FileAccess.open(mod_path + "/mods.json", FileAccess.WRITE_READ)
 	file.store_line(str(data))
+	for dir in DirAccess.get_directories_at(mod_path):
+		var modData = get_json(mod_path + "/" + dir + "/mod.json")
+		mods[modData["mod_id"]] = modData
+	print(mods)
 	file.close()
 
-
+#after the second file dialog prompt, it sets up the game path and installs/updates forgery
 func _on_file_dialog_dir_selected(dir: String) -> void:
 	print(dir)
 	game_path = dir
@@ -94,7 +109,7 @@ func _on_file_dialog_dir_selected(dir: String) -> void:
 	$Button3.visible = false
 	$TextEdit.visible = true
 
-
+#when the local app data folder is selected, it sets up the directories
 func _on_file_dialog_2_dir_selected(dir: String) -> void:
 	print(dir)
 	mod_path = dir + "/nubbys_forgery/mods"
@@ -103,10 +118,16 @@ func _on_file_dialog_2_dir_selected(dir: String) -> void:
 	$Button3.visible = true
 	$Button.visible = false
 
-
+#second button visible, prompts to select game exe folder
 func _on_button_pressed() -> void:
 	$FileDialog2.visible = true
 
-
+#first button shown, prompts to select localappdata directory
 func _on_button_3_pressed() -> void:
 	$FileDialog.visible = true
+
+#intended for testing 
+func _on_text_edit_text_changed() -> void:
+	for mod in mods.values():
+		if mod["mod_id"] == $TextEdit.text:
+			$Label.text = str(mod)
